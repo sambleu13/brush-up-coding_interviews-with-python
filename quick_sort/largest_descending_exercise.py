0e2dc7bf81ae7f77e38fd777e8318594b45ccacd
Samantha Pantoja <sambleu13@gmail.com>
2025-03-11 14:42:32 -0600

    Update correct largest_descending_exercise.py

--- quick_sort/largest_descending_exercise.py
+++ quick_sort/largest_descending_exercise.py
@@ -4,25 +4,23 @@ def find_kth_largest(numbers, k):
     if numbers:
         # implement this
         pi = partition(numbers, 0, len(numbers) - 1)
-        print(pi, k-1 , bool(pi == k-1) )
         if pi == k-1:
             return numbers[pi]
         elif k - 1 < pi:
-            return find_kth_largest(numbers[pi+1:], len(numbers)-k+1)
-        else:
             return find_kth_largest(numbers[:pi], k)
+        else:
+            return find_kth_largest(numbers[pi+1:], k - pi - 1)
         
 def partition(nums, low, high):
     # implement this
     rand_ind = random.randint(low, high)
     ind = low
     nums[low], nums[rand_ind] = nums[rand_ind], nums[low]
-    for i in range(low, high):
+    for i in range(low+1, high+1):
         if nums[i] >= nums[low]:
             ind += 1
-            nums[ind], nums[i] = nums[i], nums[ind]
+            nums[i], nums[ind] = nums[ind], nums[i]
     nums[ind], nums[low] = nums[low], nums[ind]
-    print(ind)
     return ind
 
 
