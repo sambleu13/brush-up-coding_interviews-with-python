e140763e10dacca2b34b37608e3f32c395e17e7e
Samantha Pantoja <sambleu13@gmail.com>
2025-03-11 11:13:17 -0600

    Create sort_numbers_exercise.py

---
+++ sorted_func/sort_numbers_exercise.py
@@ -0,0 +1,7 @@
+def sort_list(values):
+    # implement this
+    return sorted(values)
+
+print(sort_list([22.7, 14.3, 90.8, 45.3, 77.2]))  # Expected output: [14.3, 22.7, 45.3, 77.2, 90.8]
+print(sort_list([1, 5, 0.3, 7.6, 3.1, 100]))      # Expected output: [0.3, 1, 3.1, 5, 7.6, 100]
+print(sort_list([45, 85, 33, 90.5, 29]))          # Expected output: [29, 33, 45, 85, 90.5]
