284bc3d9f14e0955bf673b8180c808c35295e9fe
Samantha Pantoja <sambleu13@gmail.com>
2025-03-16 18:32:49 -0600

    Create best_time_to_buy_and_sell_stock.py

---
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,25 @@
+# solution using arrays
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = profit = 0
+
+        def get_profit(self, prices, base):
+            profit = 0
+            for price in prices:
+                print(prices[0], len(prices))
+                if price > base:
+                    sell_price = price - base
+                    if sell_price > profit:
+                        profit = sell_price
+            return profit
+
+
+        if len(prices) <= 1:
+            return 0
+        for i, buy_price in enumerate(prices):
+            profit = get_profit(self, prices[i:], buy_price)
+            if profit > max_profit:
+                max_profit = profit
+        return max_profit
+
+        
