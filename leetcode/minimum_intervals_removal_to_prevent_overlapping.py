6a453fa230f14c454aca6dbcdbe6e48b8d893c8c
Samantha Pantoja <sambleu13@gmail.com>
2025-03-16 23:18:22 -0600

    Update minimum_intervals_removal_to_prevent_overlapping.py

--- leetcode/minimum_intervals_removal_to_prevent_overlapping.py
+++ leetcode/minimum_intervals_removal_to_prevent_overlapping.py
@@ -1,5 +1,5 @@
 # return the mininum number of intervals to make the intervals non-overlapping
-# solution using dictionary and set
+# solution using dictionary and set, improved
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         min_ints = 0
@@ -9,11 +9,25 @@ class Solution:
             unique_ints = {tuple(i) for i in intervals}
             sort_ints = sorted(map(list,unique_ints))
             min_ints = len(intervals) - len(unique_ints)
+            max_end = sort_ints[0][0]
+            print(sort_ints, max_end)
             for interval in sort_ints:
                 if interval[0] not in ints.keys():
-                    ints[interval[0]] = interval[0]+1
+                    if ints is None:
+                        ints[interval[0]] = interval[1]
+                        max_end = interval[1]
+                    else:
+                        if interval[0] < max_end:
+                            min_ints += 1
+                        else: 
+                            ints[interval[0]] = interval[1]
+                            max_end = interval[1]
                 else:
                     if interval[1] != ints[interval[0]]:
                         min_ints += 1
+                        if interval[1] < ints[interval[0]]:
+                            ints[interval[0]] = interval[1]
+                            max_end = interval[1]
+                print(interval[0], max_end)
 
         return min_ints
