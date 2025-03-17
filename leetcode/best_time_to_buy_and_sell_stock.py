bae2362f1e588d778052fa175ce0789a6004439b
Samantha Pantoja <sambleu13@gmail.com>
2025-03-16 18:50:10 -0600

    Update to include MaxHeap in helper function

--- leetcode/best_time_to_buy_and_sell_stock.py
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -1,24 +1,24 @@
-# solution using arrays
+# solution using arrays and subhelper function with Maxheap
 # time complexity O(N^2)
+import heapq as hp
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = profit = 0
 
         def get_profit(self, prices, base):
-            profit = 0
+            profit = [0]
             for price in prices:
-                print(prices[0], len(prices))
-                if price > base:
-                    sell_price = price - base
-                    if sell_price > profit:
-                        profit = sell_price
-            return profit
+                if base < price:
+                    hp.heappush(profit, -(price - base))
+            return -hp.heappop(profit)
 
 
         if len(prices) <= 1:
             return 0
         for i, buy_price in enumerate(prices):
-            profit = get_profit(self, prices[i:], buy_price)
+            profit = get_profit(self, prices[i+1:], buy_price)
+            # print(buy_price, profit, array)
             if profit > max_profit:
                 max_profit = profit
         return max_profit
