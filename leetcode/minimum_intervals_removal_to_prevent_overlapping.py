01dab112f38e9dc764ad2d0d4f04acb8f8bd7418
Samantha Pantoja <sambleu13@gmail.com>
2025-03-16 23:40:05 -0600

    Update minimum_intervals_removal_to_prevent_overlapping.py

--- leetcode/minimum_intervals_removal_to_prevent_overlapping.py
+++ leetcode/minimum_intervals_removal_to_prevent_overlapping.py
@@ -1,5 +1,7 @@
 # return the mininum number of intervals to make the intervals non-overlapping
-# solution using dictionary and set, improved sorting by key = end interval
+# solution using dictionary and set, improved sorting by key = end interval, 
+# simplified iterations to just compare the min_ed with the start to remove
+# less intervals
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         min_ints = 0
@@ -9,25 +11,13 @@ class Solution:
             unique_ints = {tuple(i) for i in intervals}
             sort_ints = sorted(map(list,unique_ints), key=lambda x: x[1])
             min_ints = len(intervals) - len(unique_ints)
-            max_end = sort_ints[0][0]
-            print(sort_ints, max_end)
-            for interval in sort_ints:
-                if interval[0] not in ints.keys():
-                    if ints is None:
-                        ints[interval[0]] = interval[1]
-                        max_end = interval[1]
-                    else:
-                        if interval[0] < max_end:
-                            min_ints += 1
-                        else: 
-                            ints[interval[0]] = interval[1]
-                            max_end = interval[1]
+            min_end = sort_ints[0][0]
+            print(sort_ints, min_end)
+            for start, end in sort_ints:
+                if start >= min_end:
+                    min_end = end
                 else:
-                    if interval[1] != ints[interval[0]]:
-                        min_ints += 1
-                        if interval[1] < ints[interval[0]]:
-                            ints[interval[0]] = interval[1]
-                            max_end = interval[1]
-                print(interval[0], max_end)
+                    min_ints += 1
+                print(start, end, min_end)
 
         return min_ints
