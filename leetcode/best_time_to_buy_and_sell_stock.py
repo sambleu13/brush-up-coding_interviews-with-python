4166690dcc433429d12f40a2780417a6237b5381
Samantha Pantoja <sambleu13@gmail.com>
2025-03-16 21:07:09 -0600

    improve time complexity simplifying solution with one heap and implementing max function

--- leetcode/best_time_to_buy_and_sell_stock.py
+++ leetcode/best_time_to_buy_and_sell_stock.py
@@ -1,22 +1,16 @@
-# solution using arrays and subhelper function with Maxheap
-# time complexity O(N^2)
+# solution using heap and max function
+# time complexity O(N)
 import heapq as hp
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = [0]
-
-        def get_profit(self, prices, base):
-            profit = [0]
-            for price in prices:
-                if base < price:
-                    hp.heappush(profit, -(price - base))
-            return -hp.heappop(profit)
-
+        sell_price, max_profit = prices[0], [0]
 
         if len(prices) <= 1:
             return 0
-        for i, buy_price in enumerate(prices):
-            hp.heappush(max_profit, -get_profit(self, prices[i+1:], buy_price))
-            # print(buy_price, profit, array)
+
+        for price in prices[1:]:
+            hp.heappush(max_profit, -(price - sell_price))
+            sell_price = min(sell_price, price)
+            
         return -hp.heappop(max_profit)
