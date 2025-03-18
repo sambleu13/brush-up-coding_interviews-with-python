b22de220a46a9bc6de820b8270a9823d1b5110c3
Samantha Pantoja <sambleu13@gmail.com>
2025-03-18 13:29:22 -0600

    Update find_position_of_target_in_array.py

--- leetcode/find_position_of_target_in_array.py
+++ leetcode/find_position_of_target_in_array.py
@@ -1,15 +1,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        nums_set = set(nums)
-        if target not in nums_set:
-            return -1
+        # initialize search limits
         left, right = 0, len(nums)
-        while right - left > 1:
+        # while the difference between limits is positive
+        # search for the target position in nums
+        while right - left >= 1:
+            # the pivot is the middle
             pivot = (left + right) // 2
+            # if the number at the pivot position is target we return it
             if nums[pivot] == target:
                 return pivot
+            # otherwise we reduce the limits depending on the target value
+            # if its position is bigger than pivot, the left limit is pivot + 1
             if nums[pivot] < target:
                 left = pivot + 1
+            # if target's position is smaller than pivot, right limit is pivot
             else:
                 right = pivot
-        return left
+            #we repeat this reducing of the limits until the pivot is the target position
+        
+        # if we don-t find the target we return -1
+        return -1
