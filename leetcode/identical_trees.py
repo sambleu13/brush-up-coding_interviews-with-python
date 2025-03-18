c256f9d303d84a16bf9b2e87bb99101f0009cb76
Samantha Pantoja <sambleu13@gmail.com>
2025-03-18 17:56:32 -0600

    Update identical_trees.py

--- leetcode/identical_trees.py
+++ leetcode/identical_trees.py
@@ -6,34 +6,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # if both are empty
         if not p and not q:
             return True
-        if not p or not q:
+        # if one is empty or their values are different
+        if not p or not q or p.val != q.val:
             return False
-    
-        def dfs(node, visited = None):
-            if visited is None:
-                visited = []
-            if not node:
-                print('null value')
-            if node:
-                visited.append(node.val)
-                if node.left:
-                    dfs(node.left, visited)
-
-                if node.right:
-                    dfs(node.right, visited)
-            
-
-            return visited
-
-        p_lst = dfs(p)
-        q_lst = dfs(q)
-        print(p_lst, q_lst, p_lst == q_lst)
-
-        if len(p_lst) == len(q_lst):
-            if p_lst == q_lst:
-                return True
-        return False
+        # iterate recursively their left and right childes and if both are true or false, return True
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
         
