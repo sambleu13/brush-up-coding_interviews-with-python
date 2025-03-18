d8ec1314cc9d2f828bcea47909651b8d3fd269cb
Samantha Pantoja <sambleu13@gmail.com>
2025-03-18 15:31:55 -0600

    Update reverse_linked_list.py

--- leetcode/reverse_linked_list.py
+++ leetcode/reverse_linked_list.py
@@ -5,14 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def reverseHelper(current, previous):
-            if current is None:
-                return previous
+        # initialize current being the first element, and previous None
+        current = head, previous = None
+        # while there are elements in the linked list
+        while current is not None:
+            # the next node saves temporarily the next element in the original linked list
             next_node = current.next
+            # the new next current element will be the previous 
             current.next = previous
-            # print(current, previous, next_node, current.next)
-            reverseHelper(next_node, current)
-        
-        if head is None:
-            return
-        reverseHelper(head, None)
+            # the new previous element will be the current element
+            previous = current
+            # the new current element will be the temporal next element in the original list
+            current = next_node
+            # this ensures we traverse the linked list inversely 
+        # we return the first element that will be the last element set as previous
+        return previous
