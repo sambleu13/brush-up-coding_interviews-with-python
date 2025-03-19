2acbd141aafbed34a9bb0a9fcbdf7a1b02722c00
Samantha Pantoja <sambleu13@gmail.com>
2025-03-18 21:47:22 -0600

    Create two_sum_dictionary.py

---
+++ leetcode/two_sum_dictionary.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i, num in enumerate(nums):
+            complement = target-num
+            if complement in nums_dict:
+                return nums_dict[complement], i
+            nums_dict[num] = i
+        print(nums_dict)
+            
+
+
